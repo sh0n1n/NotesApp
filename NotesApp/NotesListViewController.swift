8b762967a730a0368780623136c4341797b5bb3e
Artem <dogmaster31072003@gmail.com>
2024-02-05 00:41:32 +0200

    UITableViewDataSource

--- NotesApp/NotesListViewController.swift
+++ NotesApp/NotesListViewController.swift
@@ -8,15 +8,44 @@
 import UIKit
 
 class NotesListViewController: UITableViewController {
-
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "NOTES"
+        setupTableView()
+        
     }
     
+    // MARK: - Private Methods
     private func setupTableView() {
         tableView.register(SimpleNoteTableViewCell.self, forCellReuseIdentifier: "SimpleNoteTableViewCell")
         tableView.register(ImageNoteTableViewCell.self, forCellReuseIdentifier: "ImageNoteTableViewCell")
     }
 }
 
+// MARK: - UITableViewDataSource
+extension NotesListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0,
+           let cell = tableView.dequeueReusableCell(withIdentifier: "SimpleNoteTableViewCell",
+                                                    for: indexPath) as? SimpleNoteTableViewCell {
+            return cell
+        } else if let cell = tableView.dequeueReusableCell(withIdentifier: "ImageNoteTableViewCell",
+                                                           for: indexPath) as? ImageNoteTableViewCell {
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension NotesListViewController {
+    
+}
