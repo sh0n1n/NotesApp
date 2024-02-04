7a04b1a497a8f12d940d1bc20770c14b774630b5
Artem <dogmaster31072003@gmail.com>
2024-02-04 22:40:29 +0200

    Added GUI Variables

--- NotesApp/ImageNoteTableViewCell.swift
+++ NotesApp/ImageNoteTableViewCell.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 final class ImageNoteTableViewCell: UITableViewCell {
+    // MARK: - GUI Variables
+    private let containerView: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 15
+        
+        return view
+    }()
     
+    private let attachmentView: UIImageView = {
+        let view = UIImageView()
+        
+        view.layer.cornerRadius = 15
+        view.image = UIImage(named: "image")
+        
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Title Label Text"
+        label.font = .boldSystemFont(ofSize: 20)
+        
+        return label
+    }()
 }
--- NotesApp/SimpleNoteTableViewCell.swift
+++ NotesApp/SimpleNoteTableViewCell.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 final class SimpleNoteTableViewCell: UITableViewCell {
+    // MARK: - GUI Variables
+    private let containerView: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 15
+        
+        return view
+    }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Title Label Text"
+        label.font = .boldSystemFont(ofSize: 20)
+        
+        return label
+    }()
 }
